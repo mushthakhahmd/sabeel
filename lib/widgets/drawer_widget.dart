c956147643034507aeb3eae8f735c3e592fcd386
vaisakhferoke <vaisakhperumthody@gmail.com>
2024-07-06 16:54:28 +0530

    new build

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:provider/provider.dart';
 import 'package:sabeel/about_us.dart';
 import 'package:sabeel/home.dart';
